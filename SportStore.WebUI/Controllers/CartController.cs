da504e3f795e26588ce15cd26ebb5d5c2a55d16a
nelson820125 <lining820125@163.com>
2015-06-05 17:05:20 +0800

    cart

--- SportStore.WebUI/Controllers/CartController.cs
+++ SportStore.WebUI/Controllers/CartController.cs
@@ -46,5 +46,14 @@ namespace SportStore.WebUI.Controllers
             }
             return RedirectToAction("Index", new { returnUrl });
         }
+
+        public PartialViewResult Summary(Cart cart) {
+            return PartialView(cart);
+        }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
     }
 }
